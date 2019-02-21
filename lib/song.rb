1453be38e4d52e5703040734034a27834a9713e3
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-21 21:24:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
 
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << @genre
   end
 
   def self.count
@@ -15,7 +17,8 @@ class Song
   end
 
   def self.genres
-    @@genres = []
+
+    
   end
 
 
